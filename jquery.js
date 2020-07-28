27f3cb21aac92241a6fdfcf820ed619d9efd230e
Joseph Scothmer-Clay <josephscoth@gmail.com>
2020-07-28 10:51:31 +0100

    new

--- jquery.js
+++ jquery.js
@@ -1,8 +1,11 @@
-jQuery(document).ready(function() {
-  $(function(){
-    $("#title").hide().delay(500).slideDown(1000);
-    $("#sub-title").hide().delay(1500).slideDown(1000);
-    $("#title-hr").hide().delay(2500).animate({width: 'toggle'},1500);
+$(window).on("load", function() {
+  $(".loader").fadeOut(1000);
+  jQuery(document).ready(function() {
+    $(function(){
+      $("#title").hide().delay(1000).slideDown(1000);
+      $("#sub-title").hide().delay(2000).slideDown(1000);
+      $("#title-hr").hide().delay(3000).animate({width: 'toggle'},1500);
+    });
   });
 
 });
